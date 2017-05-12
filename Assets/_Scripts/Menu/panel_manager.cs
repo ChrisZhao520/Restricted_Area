184589ace9bbd3bf0624689e0932037b238548f8
zhaoyunpeng <741092665@qq.com>
2017-05-12 13:58:28 +0800

    Assets

---
+++ Assets/_Scripts/Menu/panel_manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_manager : MonoBehaviour {
+	public Animator startButton;
+	public Animator loadlistButton;
+	public Animator loadpanleButton;
+	public Animator setlistButton;
+	public Animator helplistButton;
+	public Animator aboutlistButton;
+	public Animator setpanleButton;
+	public Animator helppanleButton;
+	public Animator aboutpanleButton;
+	public GameObject audios;
+	public GameObject lights;
+	public GameObject cameras;
+	// Use this for initialization
+
+	public void openload () {
+		startButton.SetBool ("ishidden",true);
+		loadlistButton.SetBool ("ishidden", false);
+		loadpanleButton.SetBool ("ishidden", false);
+	}
+    public void closedload()
+    {
+        startButton.SetBool("ishidden", false);
+        loadlistButton.SetBool("ishidden", true);
+        loadpanleButton.SetBool("ishidden", true);
+    }
+
+	public void openset(){
+		startButton.SetBool ("ishidden",true);
+		setlistButton.SetBool ("ishidden", false);
+		setpanleButton.SetBool ("ishidden", false);
+	}
+	public void openhelp(){
+		startButton.SetBool ("ishidden",true);
+		helplistButton.SetBool ("ishidden", false);
+		helppanleButton.SetBool ("ishidden", false);
+	}
+	public void openabout(){
+		startButton.SetBool ("ishidden",true);
+		aboutlistButton.SetBool ("ishidden", false);
+		aboutpanleButton.SetBool ("ishidden", false);
+	}
+	
+	public void closedset(){
+		startButton.SetBool ("ishidden",false);
+		setlistButton.SetBool ("ishidden", true);
+		setpanleButton.SetBool ("ishidden", true);
+	}
+	public void closedhelp(){
+		startButton.SetBool ("ishidden",false);
+		helplistButton.SetBool ("ishidden", true);
+		helppanleButton.SetBool ("ishidden", true);
+	}
+	public void closedabout(){
+		startButton.SetBool ("ishidden",false);
+		aboutlistButton.SetBool ("ishidden", true);
+		aboutpanleButton.SetBool ("ishidden", true);
+	}
+	public void openaudios(){
+		audios.gameObject.SetActive (true);
+		lights.gameObject.SetActive (false);
+		cameras.gameObject.SetActive (false);
+	}
+	public void openlights(){
+		audios.gameObject.SetActive (false);
+		lights.gameObject.SetActive (true);
+		cameras.gameObject.SetActive (false);
+	}
+	public void opencameras(){
+		audios.gameObject.SetActive (false);
+		lights.gameObject.SetActive (false);
+		cameras.gameObject.SetActive (true);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/Welcome/Sliderbj.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Sliderbj : MonoBehaviour {
+    private bool bl = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(WaitAndPrint1(2.0f));
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (bl) { 
+            if (GetComponent<Slider>().value <= 0.6f)
+            {
+                GetComponent<Slider>().value += Time.deltaTime * 0.2f;
+            }
+            if (GetComponent<Slider>().value > 0.6f && GetComponent<Slider>().value < 0.7f)
+            {
+                GetComponent<Slider>().value += Time.deltaTime * 0.05f;
+            }
+            if (GetComponent<Slider>().value > 0.7f)
+            {
+                GetComponent<Slider>().value += Time.deltaTime * 0.5f;
+            }
+            if (GetComponent<Slider>().value >= 1)
+            {
+                StartCoroutine(WaitAndPrint2(2.0f));
+            }
+        }
+	}
+
+    public void ToMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    IEnumerator WaitAndPrint1(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //等待之后执行的动作  
+        bl = true;
+    }
+    IEnumerator WaitAndPrint2(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //等待之后执行的动作  
+        ToMenuScene();
+    }
+}
