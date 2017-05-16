74e03836ffc1bfacd7676365100403ac3ad538c7
zhaoyunpeng <741092665@qq.com>
2017-05-16 17:22:52 +0800

    Backpack

---
+++ zhaoyunpeng/Assets/beibao/script/model/Itemmodel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Itemmodel 
+{
+	private static Dictionary<string,item> GridItem = new Dictionary<string,item> ();
+    public static void StoreItem(string name, item item)
+    {
+		if (GridItem.ContainsKey (name)) {
+            DeleteItem(name);
+		}
+        GridItem.Add(name, item);
+	}
+    public static item GetItem(string name)
+    {
+        if (GridItem.ContainsKey(name))
+        {
+            return GridItem[name];
+        }
+        else
+        {
+            return null;
+        }
+    }
+	public static void DeleteItem(string name){
+        if (GridItem.ContainsKey(name))
+        {
+            GridItem.Remove(name);
+        }
+	}
+
+}
---
+++ zhaoyunpeng/Assets/beibao/script/view/Beibaoyiman.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Beibaoyiman : MonoBehaviour {
+
+    
+    public Text ContentText;
+
+    public void Updatexianshi(string text)
+    {
+        ContentText.text = text;
+    }
+    public void show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void hidden()
+    {
+        gameObject.SetActive(false);
+    }
+}
