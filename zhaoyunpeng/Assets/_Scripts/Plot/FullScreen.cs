ed8c2068a444ea319bdd4005a25ef4e2eb80eca5
zhaoyunpeng <741092665@qq.com>
2017-05-25 12:13:46 +0800

    完善剧情

---
+++ zhaoyunpeng/Assets/_Scripts/Plot/FullScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+
+    void Start()
+    {
+        //得到主Camera
+        Camera cam = Camera.main;
+        //设置为正交
+        cam.orthographic = true;
+        //设置大小为屏幕宽度的一半。乘以10是为了以后使用物体缩放时直接使用像素
+        cam.orthographicSize = Screen.height / 2 * 10;
+        //设置背景Plane的大小
+        gameObject.transform.localScale = new Vector3(cam.pixelWidth, 1, cam.pixelHeight);
+    }
+
+    //以后Plane上的物体都使用此缩放
+    static public Vector3 GetScale()
+    {
+        Camera cam = Camera.main;
+        return new Vector3(cam.pixelHeight, 1, cam.pixelHeight);
+    }
+}
---
+++ zhaoyunpeng/Assets/_Scripts/Plot/VideoScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class VideoScript : MonoBehaviour
+{
+    //电影纹理  
+    public MovieTexture movTexture;
+    public float movieLength;
+    public GameObject graduallyActive;
+
+    private float timer;
+    private float t;
+
+    void Start()
+    {
+        //设置当前对象的主纹理为电影纹理
+        gameObject.GetComponent<Renderer>().material.mainTexture = movTexture;
+
+        //设置电影纹理播放模式为循环  
+        movTexture.loop = false;
+
+        Screen.lockCursor = true;
+        timer = 0;
+        t = 0;
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+        if (timer >= movieLength)
+        {
+            t += Time.deltaTime * 0.5f;
+            graduallyActive.GetComponent<Image>().color = new Color(0, 0, 0, t);
+            gameObject.GetComponent<AudioSource>().volume = 1 - t;
+            if (t >= 2)
+            {
+                movTexture.Stop();
+                SceneManager.LoadScene("Main");
+            }
+            
+        }
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            movTexture.Stop();
+            SceneManager.LoadScene("Main");
+
+        }
+        
+    }
+
+    void OnGUI()
+    {
+        movTexture.Play();
+        /*if (!movTexture.isPlaying)
+        {
+            SceneManager.LoadScene("Main");
+        }*/
+    }
+}
